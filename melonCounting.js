17556276b593232648064a35046ef6a8f7ec1f5c
Melissa Keller <melissa.keller23@gmail.com>
2021-10-18 17:05:53 -0500

    adding js file that converted python function to js

---
+++ melonCounting.js
@@ -0,0 +1,25 @@
+melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+const count_melons = melon_list => {
+    const melon_counts = {};
+
+    for (const melon of melon_list){
+        if (!melon_counts.hasOwnProperty(melon)){
+            melon_counts[melon] = 1;
+        }
+        else{
+            melon_counts[melon] = melon_counts[melon] + 1;
+        }
+    }
+    return melon_counts;
+}
